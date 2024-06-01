65827a93ebb1f23cb48911bbeab5d9fe24ed0a50
Mikel <136650249+C2kAdmin@users.noreply.github.com>
2024-05-31 20:32:37 -0400

    Add files via upload

---
+++ system_v3.5.2/mod1/mod1.php
@@ -0,0 +1,9 @@
+<!-- mod1.php -->
+<link rel="stylesheet" href="mod1/styles_mod1.css?<?php echo filemtime('styles_mod1.css'); ?>">
+
+<div class="modulo-1">
+    <div class="mod1-columna-1">
+        <h2>Columna 1</h2>
+        <p>Este es un texto de ejemplo en el que podemos visualizar el contenido referencial de la columna 1 del módulo 1.</p>
+    </div>
+</div>
---
+++ system_v3.5.2/mod2/mod2.php
@@ -0,0 +1,13 @@
+<!-- mod2.php -->
+<link rel="stylesheet" href="mod2/styles_mod2.css?<?php echo filemtime('mod2/styles_mod2.css'); ?>">
+
+<div class="modulo-2">
+    <div class="mod2-columna-1">
+        <h2>Columna 1</h2>
+        <p>Este es un texto de ejemplo en el que podemos visualizar el contenido referencial de la columna 1 del módulo 2.</p>
+    </div>
+    <div class="mod2-columna-2">
+        <h2>Columna 2</h2>
+        <p>Este es un texto de ejemplo en el que podemos visualizar el contenido referencial de la columna 2 del módulo 2.</p>
+    </div>
+</div>
